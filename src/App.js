ec500e4b51d2a9413fd796aabc448a2aba83e258
Mahjabeen Sultana <mahjabeen.sultana2012@gmail.com>
2019-11-22 12:36:04 -0600

    add HomePage Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import './App.css';
+import HomePage from './HomePage.component';
 
 function App() {
   return (
     <div className="App">
       <h2>Welcome to our Shop</h2>
+      <HomePage />
     </div>
   );
 }
