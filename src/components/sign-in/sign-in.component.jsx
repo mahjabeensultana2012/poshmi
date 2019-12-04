762379a3d16419f9e5ffe2e15c0b738033c88727
Mahjabeen Sultana <mahjabeen.sultana2012@gmail.com>
2019-12-03 19:46:13 -0600

    display signin form

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -9,18 +9,36 @@ class SignIn extends Component {
       password: '',
     };
   }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.setState = { email: '', password: '' };
+  };
+
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState = { [name]: value };
+  };
+
   render() {
     return (
       <div className="sign-in">
         <h2>Already have an account</h2>
         <span>Sign in with your email and password</span>
-        <form>
-          <input name="email" type="email" value={this.state.email} required />
+        <form onSubmit={this.handleSubmit}>
+          <input
+            name="email"
+            type="email"
+            value={this.state.email}
+            onChange={this.handleChange}
+            required
+          />
           <label>Email</label>
           <input
             name="password"
             type="password"
             value={this.state.password}
+            onChange={this.handleChange}
             required
           />
           <label>Password</label>
