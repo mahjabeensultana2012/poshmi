de8dbec855bb8d6d94ff6ea30572aca3f00d0f52
Mahjabeen Sultana <mahjabeen.sultana2012@gmail.com>
2019-12-03 18:04:02 -0600

    add sign-in form

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -14,7 +14,21 @@ class SignIn extends Component {
       <div className="sign-in">
         <h2>Already have an account</h2>
         <span>Sign in with your email and password</span>
+        <form>
+          <input name="email" type="email" value={this.state.email} required />
+          <label>Email</label>
+          <input
+            name="password"
+            type="password"
+            value={this.state.password}
+            required
+          />
+          <label>Password</label>
+          <input type="submit" value="submit" />
+        </form>
       </div>
     );
   }
 }
+
+export default SignIn;
