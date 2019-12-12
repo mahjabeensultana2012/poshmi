fe7fa77b43f56a83e26888d314042ade664f3c8f
Mahjabeen Sultana <mahjabeen.sultana2012@gmail.com>
2019-12-12 10:06:50 -0600

    create usestore

--- src/index.js
+++ src/index.js
@@ -5,9 +5,10 @@ import App from './App';
 
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import store from './redux/store';
 //ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
