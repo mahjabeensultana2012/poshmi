87d8d66faa43cc71e04568f8a018219f4d8ea5a8
Mahjabeen Sultana <mahjabeen.sultana2012@gmail.com>
2019-12-12 21:42:47 -0600

    create cart component and styles for it

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import CartIcon from '../cart-icon/cart-icon.component';
 
 import { ReactComponent as Logo } from '../../assets/logo.svg';
 
@@ -29,6 +30,7 @@ const Header = ({ currentUser }) => (
           SIGN IN
         </Link>
       )}
+      <CartIcon />
     </div>
   </div>
 );
