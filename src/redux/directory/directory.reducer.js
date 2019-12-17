648ebcb48ce58a3b806351f571d550cc0e0be220
Mahjabeen Sultana <mahjabeen.sultana2012@gmail.com>
2019-12-16 18:08:59 -0600

    implemeement data normalization for collection component page

--- src/redux/directory/directory.reducer.js
+++ src/redux/directory/directory.reducer.js
@@ -2,33 +2,35 @@ const INITITIAL_STATE = {
   sections: [
     {
       title: 'hats',
-      //imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
       imageUrl: 'https://i.ibb.co/H2S3kMp/images.jpg',
       id: 1,
+      linkUrl: 'shop/hats',
     },
     {
       title: 'jackets',
       imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
       id: 2,
+      linkUrl: 'shop/jackets',
     },
     {
       title: 'sneakers',
       imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
       id: 3,
+      linkUrl: 'shop/sneakers',
     },
     {
       title: 'women',
-      //imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
       imageUrl: 'https://i.ibb.co/BrxBL6K/women.jpg',
       size: 'large',
       id: 4,
+      linkUrl: 'shop/women',
     },
     {
       title: 'men',
-      //imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
       imageUrl: 'https://i.ibb.co/Dwsgb57/men.jpg',
       size: 'large',
       id: 5,
+      linkUrl: 'shop/men',
     },
   ],
 };
