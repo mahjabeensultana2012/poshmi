b2c696d1380509361b817d98a2e46cc736be2a82
Mahjabeen Sultana <mahjabeen.sultana2012@gmail.com>
2019-12-20 17:44:56 -0600

    correction in menu-item.component

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -13,6 +13,7 @@ const Directory = ({ sections }) => (
         title={section.title}
         imageUrl={section.imageUrl}
         size={section.size}
+        linkUrl={section.linkUrl}
       />
     ))}
   </div>
