4da30da8dab87a676121ec00f91a500d3910f976
Mahjabeen Sultana <mahjabeen.sultana2012@gmail.com>
2020-01-15 16:46:04 -0600

    Updates

---
+++ precache-manifest.80a25c445638fda4ca97d375393f5d31.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41162cb2d3e25abff6c0f47fb9829c9e",
+    "url": "/poshmi/index.html"
+  },
+  {
+    "revision": "0c587d30f330f5143f78",
+    "url": "/poshmi/static/css/main.4946bfed.chunk.css"
+  },
+  {
+    "revision": "6ba2a01d598028ee61da",
+    "url": "/poshmi/static/js/2.3966757d.chunk.js"
+  },
+  {
+    "revision": "dc31af278e2778356832223a9c8f7529",
+    "url": "/poshmi/static/js/2.3966757d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0c587d30f330f5143f78",
+    "url": "/poshmi/static/js/main.dea75509.chunk.js"
+  },
+  {
+    "revision": "fd586af572f1492b49ce",
+    "url": "/poshmi/static/js/runtime-main.6be218a3.js"
+  },
+  {
+    "revision": "2e95356ad0ec6049a5f8138e09027d97",
+    "url": "/poshmi/static/media/logo.2e95356a.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/poshmi/static/media/shopping-bag.30f07071.svg"
+  }
+]);
